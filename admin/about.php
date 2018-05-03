42a34c7268899898ed619ac6ddb9089107df5bc8
Rascarquapac <Thierry@192.168.1.4>
2018-05-03 11:15:23 +0200

    Adding copyright (..)

--- admin/about.php
+++ admin/about.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2004-2017 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2018 Admin <thierry.nancy@gmail.com>
+ * Copyright (C) 2018 Thierry Nancy <thierry.nancy@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
