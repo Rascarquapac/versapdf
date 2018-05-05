b6d7957270083970317c5d726bb338e934ec34b4
Rascarquapac <Thierry@192.168.1.4>
2018-05-05 17:47:22 +0200

    Test file with form defining parameters values

--- versapdfindex.php
+++ versapdfindex.php
@@ -62,175 +62,222 @@ $now=dol_now();
 /*
  * Actions
  */
+/*
+include DOL_DOCUMENT_ROOT.'/core/actions_setmoduleoptions.inc.php';
+if ($action == 'updateall')
+{
+    $db->begin();
+    $res1=$res2=$res3=$res4=$res5=$res6=0;
+    $res1=dolibarr_set_const($db, 'ADHERENT_LOGIN_NOT_REQUIRED', GETPOST('ADHERENT_LOGIN_NOT_REQUIRED', 'alpha'), 'chaine', 0, '', $conf->entity);
+    $res2=dolibarr_set_const($db, 'ADHERENT_MAIL_REQUIRED', GETPOST('ADHERENT_MAIL_REQUIRED', 'alpha'), 'chaine', 0, '', $conf->entity);
+    $res3=dolibarr_set_const($db, 'ADHERENT_DEFAULT_SENDINFOBYMAIL', GETPOST('ADHERENT_DEFAULT_SENDINFOBYMAIL', 'alpha'), 'chaine', 0, '', $conf->entity);
+    $res4=dolibarr_set_const($db, 'ADHERENT_BANK_USE', GETPOST('ADHERENT_BANK_USE', 'alpha'), 'chaine', 0, '', $conf->entity);
+    // Use vat for invoice creation
+    if ($conf->facture->enabled)
+    {
+        $res4=dolibarr_set_const($db, 'ADHERENT_VAT_FOR_SUBSCRIPTIONS', GETPOST('ADHERENT_VAT_FOR_SUBSCRIPTIONS', 'alpha'), 'chaine', 0, '', $conf->entity);
+        $res5=dolibarr_set_const($db, 'ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS', GETPOST('ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS', 'alpha'), 'chaine', 0, '', $conf->entity);
+        if (! empty($conf->product->enabled) || ! empty($conf->service->enabled))
+        {
+            $res6=dolibarr_set_const($db, 'ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS', GETPOST('ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS', 'alpha'), 'chaine', 0, '', $conf->entity);
+        }
+    }
+    if ($res1 < 0 || $res2 < 0 || $res3 < 0 || $res4 < 0 || $res5 < 0 || $res6 < 0)
+    {
+        setEventMessages('ErrorFailedToSaveDate', null, 'errors');
+        $db->rollback();
+    }
+    else
+    {
+        setEventMessages('RecordModifiedSuccessfully', null, 'mesgs');
+        $db->commit();
+    }
+}
 
-// None
+// Action mise a jour ou ajout d'une constante
+if ($action == 'update' || $action == 'add')
+{
+    $constname=GETPOST('constname','alpha');
+    $constvalue=(GETPOST('constvalue_'.$constname) ? GETPOST('constvalue_'.$constname) : GETPOST('constvalue'));
+    
+    if (($constname=='ADHERENT_CARD_TYPE' || $constname=='ADHERENT_ETIQUETTE_TYPE' || $constname=='ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS') && $constvalue == -1) $constvalue='';
+    if ($constname=='ADHERENT_LOGIN_NOT_REQUIRED') // Invert choice
+    {
+        if ($constvalue) $constvalue=0;
+        else $constvalue=1;
+    }
+    
+    $consttype=GETPOST('consttype','alpha');
+    $constnote=GETPOST('constnote');
+    $res=dolibarr_set_const($db,$constname,$constvalue,$type[$consttype],0,$constnote,$conf->entity);
+    
+    if (! $res > 0) $error++;
+    
+    if (! $error)
+    {
+        setEventMessages($langs->trans("SetupSaved"), null, 'mesgs');
+    }
+    else
+    {
+        setEventMessages($langs->trans("Error"), null, 'errors');
+    }
+}
+
+// Action activation d'un sous module du module adherent
+if ($action == 'set')
+{
+    $result=dolibarr_set_const($db, GETPOST('name','alpha'),GETPOST('value'),'',0,'',$conf->entity);
+    if ($result < 0)
+    {
+        print $db->error();
+    }
+}
+
+// Action desactivation d'un sous module du module adherent
+if ($action == 'unset')
+{
+    $result=dolibarr_del_const($db,GETPOST('name','alpha'),$conf->entity);
+    if ($result < 0)
+    {
+        print $db->error();
+    }
+}
+*/
 
 
 /*
  * View
  */
+// Libraries
+$h = 0;
+$head = array();
+
+$head[$h][0] = dol_buildpath("/custom/versapdf/parameters1_page.php", 1);
+$head[$h][1] = $langs->trans("Parameters 1");
+$head[$h][2] = 'settings';
+$h++;
+$head[$h][0] = dol_buildpath("/custom/versapdf/parameters2_page.php", 1);
+$head[$h][1] = $langs->trans("Parameters 2");
+$head[$h][2] = 'about';
+$h++;
+
+
+require_once DOL_DOCUMENT_ROOT  . "/core/lib/admin.lib.php";
+require_once './lib/versapdf.lib.php';
 
 $form = new Form($db);
 $formfile = new FormFile($db);
 
 llxHeader("",$langs->trans("versapdfArea"));
+dol_fiche_head(
+    $head,
+    'Parameters 1',
+    $langs->trans("versapdf"),
+    0,
+    "versapdf@versapdf"
+    );
 
 print load_fiche_titre($langs->trans("versapdfArea"),'','versapdf.png@versapdf');
 
 print '<div class="fichecenter"><div class="fichethirdleft">';
 
 
-/* BEGIN MODULEBUILDER DRAFT MYOBJECT
-// Draft MyObject
-if (! empty($conf->versapdf->enabled) && $user->rights->versapdf->read)
-{
-	$langs->load("orders");
-
-	$sql = "SELECT c.rowid, c.ref, c.ref_client, c.total_ht, c.tva as total_tva, c.total_ttc, s.rowid as socid, s.nom as name, s.client, s.canvas";
-    $sql.= ", s.code_client";
-	$sql.= " FROM ".MAIN_DB_PREFIX."commande as c";
-	$sql.= ", ".MAIN_DB_PREFIX."societe as s";
-	if (! $user->rights->societe->client->voir && ! $socid) $sql.= ", ".MAIN_DB_PREFIX."societe_commerciaux as sc";
-	$sql.= " WHERE c.fk_soc = s.rowid";
-	$sql.= " AND c.fk_statut = 0";
-	$sql.= " AND c.entity IN (".getEntity('commande').")";
-	if (! $user->rights->societe->client->voir && ! $socid) $sql.= " AND s.rowid = sc.fk_soc AND sc.fk_user = " .$user->id;
-	if ($socid)	$sql.= " AND c.fk_soc = ".$socid;
-
-	$resql = $db->query($sql);
-	if ($resql)
-	{
-		$total = 0;
-		$num = $db->num_rows($resql);
-
-		print '<table class="noborder" width="100%">';
-		print '<tr class="liste_titre">';
-		print '<th colspan="3">'.$langs->trans("DraftOrders").($num?' <span class="badge">'.$num.'</span>':'').'</th></tr>';
-
-		$var = true;
-		if ($num > 0)
-		{
-			$i = 0;
-			while ($i < $num)
-			{
-
-				$obj = $db->fetch_object($resql);
-				print '<tr class="oddeven"><td class="nowrap">';
-                $orderstatic->id=$obj->rowid;
-                $orderstatic->ref=$obj->ref;
-                $orderstatic->ref_client=$obj->ref_client;
-                $orderstatic->total_ht = $obj->total_ht;
-                $orderstatic->total_tva = $obj->total_tva;
-                $orderstatic->total_ttc = $obj->total_ttc;
-                print $orderstatic->getNomUrl(1);
-                print '</td>';
-				print '<td class="nowrap">';
-				$companystatic->id=$obj->socid;
-				$companystatic->name=$obj->name;
-				$companystatic->client=$obj->client;
-                $companystatic->code_client = $obj->code_client;
-                $companystatic->code_fournisseur = $obj->code_fournisseur;
-                $companystatic->canvas=$obj->canvas;
-				print $companystatic->getNomUrl(1,'customer',16);
-				print '</td>';
-				print '<td align="right" class="nowrap">'.price($obj->total_ttc).'</td></tr>';
-				$i++;
-				$total += $obj->total_ttc;
-			}
-			if ($total>0)
-			{
-
-				print '<tr class="liste_total"><td>'.$langs->trans("Total").'</td><td colspan="2" align="right">'.price($total)."</td></tr>";
-			}
-		}
-		else
-		{
-
-			print '<tr class="oddeven"><td colspan="3" class="opacitymedium">'.$langs->trans("NoOrder").'</td></tr>';
-		}
-		print "</table><br>";
-
-		$db->free($resql);
-	}
-	else
-	{
-		dol_print_error($db);
-	}
-}
-END MODULEBUILDER DRAFT MYOBJECT */
 
 
-print '</div><div class="fichetwothirdright"><div class="ficheaddleft">';
 
-
-$NBMAX=3;
-$max=3;
-
-/* BEGIN MODULEBUILDER LASTMODIFIED MYOBJECT
-// Last modified myobject
-if (! empty($conf->versapdf->enabled) && $user->rights->versapdf->read)
-{
-	$sql = "SELECT s.rowid, s.nom as name, s.client, s.datec, s.tms, s.canvas";
-    $sql.= ", s.code_client";
-	$sql.= " FROM ".MAIN_DB_PREFIX."societe as s";
-	if (! $user->rights->societe->client->voir && ! $socid) $sql.= ", ".MAIN_DB_PREFIX."societe_commerciaux as sc";
-	$sql.= " WHERE s.client IN (1, 2, 3)";
-	$sql.= " AND s.entity IN (".getEntity($companystatic->element, 1).")";
-	if (! $user->rights->societe->client->voir && ! $socid) $sql.= " AND s.rowid = sc.fk_soc AND sc.fk_user = " .$user->id;
-	if ($socid)	$sql.= " AND s.rowid = $socid";
-	$sql .= " ORDER BY s.tms DESC";
-	$sql .= $db->plimit($max, 0);
-
-	$resql = $db->query($sql);
-	if ($resql)
-	{
-		$num = $db->num_rows($resql);
-		$i = 0;
-
-		print '<table class="noborder" width="100%">';
-		print '<tr class="liste_titre">';
-		print '<th colspan="2">';
-		if (empty($conf->global->SOCIETE_DISABLE_PROSPECTS) && empty($conf->global->SOCIETE_DISABLE_CUSTOMERS)) print $langs->trans("BoxTitleLastCustomersOrProspects",$max);
-        else if (! empty($conf->global->SOCIETE_DISABLE_CUSTOMERS)) print $langs->trans("BoxTitleLastModifiedProspects",$max);
-		else print $langs->trans("BoxTitleLastModifiedCustomers",$max);
-		print '</th>';
-		print '<th align="right">'.$langs->trans("DateModificationShort").'</th>';
-		print '</tr>';
-		if ($num)
-		{
-			while ($i < $num)
-			{
-				$objp = $db->fetch_object($resql);
-				$companystatic->id=$objp->rowid;
-				$companystatic->name=$objp->name;
-				$companystatic->client=$objp->client;
-                $companystatic->code_client = $objp->code_client;
-                $companystatic->code_fournisseur = $objp->code_fournisseur;
-                $companystatic->canvas=$objp->canvas;
-				print '<tr class="oddeven">';
-				print '<td class="nowrap">'.$companystatic->getNomUrl(1,'customer',48).'</td>';
-				print '<td align="right" nowrap>';
-				print $companystatic->getLibCustProspStatut();
-				print "</td>";
-				print '<td align="right" nowrap>'.dol_print_date($db->jdate($objp->tms),'day')."</td>";
-				print '</tr>';
-				$i++;
-
-
-			}
-
-			$db->free($resql);
-		}
-		else
-		{
-			print '<tr class="oddeven"><td colspan="3" class="opacitymedium">'.$langs->trans("None").'</td></tr>';
-		}
-		print "</table><br>";
-	}
-}
-*/
+/**/
 
 print '</div></div></div>';
+print '<br>';
+
+
+
+// Setup page goes here
+echo $langs->trans("versapdfPage");
+
+$form = new Form($db);
+
+$help_url='EN:Module_Foundations|FR:Module_Adh&eacute;rents|ES:M&oacute;dulo_Miembros';
+
+print '<form action="'.$_SERVER["PHP_SELF"].'" method="POST">';
+print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+print '<input type="hidden" name="action" value="updateall">';
+
+print load_fiche_titre($langs->trans("MemberMainOptions"),'','');
+print '<table class="noborder" width="100%">';
+print '<tr class="liste_titre">';
+print '<td>'.$langs->trans("Description").'</td>';
+print '<td>'.$langs->trans("Value").'</td>';
+print "</tr>\n";
+
+// Login/Pass required for members
+print '<tr class="oddeven"><td>'.$langs->trans("AdherentLoginRequired").'</td><td>';
+print $form->selectyesno('ADHERENT_LOGIN_NOT_REQUIRED',(! empty($conf->global->ADHERENT_LOGIN_NOT_REQUIRED)?0:1),1);
+print "</td></tr>\n";
+
+// Mail required for members
+print '<tr class="oddeven"><td>'.$langs->trans("AdherentMailRequired").'</td><td>';
+print $form->selectyesno('ADHERENT_MAIL_REQUIRED',(! empty($conf->global->ADHERENT_MAIL_REQUIRED)?$conf->global->ADHERENT_MAIL_REQUIRED:0),1);
+print "</td></tr>\n";
+
+// Send mail information is on by default
+print '<tr class="oddeven"><td>'.$langs->trans("MemberSendInformationByMailByDefault").'</td><td>';
+print $form->selectyesno('ADHERENT_DEFAULT_SENDINFOBYMAIL',(! empty($conf->global->ADHERENT_DEFAULT_SENDINFOBYMAIL)?$conf->global->ADHERENT_DEFAULT_SENDINFOBYMAIL:0),1);
+print "</td></tr>\n";
+
+// Insert subscription into bank account
+print '<tr class="oddeven"><td>'.$langs->trans("MoreActionsOnSubscription").'</td>';
+$arraychoices=array('0'=>$langs->trans("None"));
+if (! empty($conf->banque->enabled)) $arraychoices['bankdirect']=$langs->trans("MoreActionBankDirect");
+if (! empty($conf->banque->enabled) && ! empty($conf->societe->enabled) && ! empty($conf->facture->enabled)) $arraychoices['invoiceonly']=$langs->trans("MoreActionInvoiceOnly");
+if (! empty($conf->banque->enabled) && ! empty($conf->societe->enabled) && ! empty($conf->facture->enabled)) $arraychoices['bankviainvoice']=$langs->trans("MoreActionBankViaInvoice");
+print '<td>';
+print $form->selectarray('ADHERENT_BANK_USE',$arraychoices,$conf->global->ADHERENT_BANK_USE,0);
+print '</td>';
+print "</tr>\n";
+
+// Use vat for invoice creation
+if ($conf->facture->enabled)
+{
+    print '<tr class="oddeven"><td>'.$langs->trans("VATToUseForSubscriptions").'</td>';
+    if (! empty($conf->banque->enabled))
+    {
+        print '<td>';
+        print $form->selectarray('ADHERENT_VAT_FOR_SUBSCRIPTIONS', array('0'=>$langs->trans("NoVatOnSubscription"),'defaultforfoundationcountry'=>$langs->trans("Default")), (empty($conf->global->ADHERENT_VAT_FOR_SUBSCRIPTIONS)?'0':$conf->global->ADHERENT_VAT_FOR_SUBSCRIPTIONS), 0);
+        print '</td>';
+    }
+    else
+    {
+        print '<td align="right">';
+        print $langs->trans("WarningModuleNotActive",$langs->transnoentities("Module85Name"));
+        print '</td>';
+    }
+    print "</tr>\n";
+    
+    if (! empty($conf->product->enabled) || ! empty($conf->service->enabled))
+    {
+        print '<tr class="oddeven"><td>'.$langs->trans("ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS").'</td>';
+        print '<td>';
+        $form->select_produits($conf->global->ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS, 'ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS');
+        print '</td>';
+    }
+    print "</tr>\n";
+}
+
+print '</table>';
+
+print '<center>';
+print '<input type="submit" class="button" value="'.$langs->trans("Update").'" name="Button">';
+print '</center>';
+
+print '</form>';
+
+print '<br>';
+
+//END TN test
+
+print '</div></div></div>';
+
 
 llxFooter();
 
